760c7f0cf90225aa2aed63e030e1358e72bba924
Adam Woodcock <adam.woodcock98@gmail.com>
2022-03-02 16:09:27 +0000

    Committing unsaved changes to journey_spec

--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -3,3 +3,30 @@ require 'journey.rb'
 describe Journey do
 
 end
+
+it 'forgets entry station when touched out' do
+  subject.touch_out(:exit_station)
+  expect(subject.entry_station).to eq nil
+end
+
+it 'saves exit station when touched out' do
+  subject.touch_out(:exit_station)
+  expect(subject.exit_station).to eq :exit_station
+end
+
+"touch in describe"
+
+it 'should return true if touched in' do
+  expect(subject).to be_in_journey
+end
+
+it 'should return false if touched out' do
+  subject.touch_out(:exit_station)
+  expect(subject).to_not be_in_journey
+end
+
+it 'after touched in saves the entry station' do
+  subject.top_up(1)
+  subject.touch_in(:entry_station)
+  expect(subject.entry_station).to eq :entry_station
+end
